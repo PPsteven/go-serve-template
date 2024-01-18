73a6e38b8846aed8efff6b70884639aef5261089
ppsteven <ppsteven@outlook.com>
2024-01-18 14:40:50 +0800

    new feature: log

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,30 +1,15 @@
 package db
 
 import (
-	"fmt"
-	log "github.com/sirupsen/logrus"
-	"go-server-template/internal/conf"
-	"go-server-template/internal/model"
 	"gorm.io/gorm"
 )
 
 var dB *gorm.DB
 
-func Init(d *gorm.DB) {
+func InitDB(d *gorm.DB) {
 	dB = d
-	err := AutoMigrate(new(model.User))
-	if err != nil {
-		log.Fatalf("failed migrate database: %s", err.Error())
-	}
 }
 
-func AutoMigrate(dist ...interface{}) error {
-	var err error
-	if conf.Conf.Database.Type == "mysql" {
-		// TODO ...
-	} else {
-		err = dB.AutoMigrate(dist...)
-	}
-	fmt.Println(123)
-	return err
+func GetDB() *gorm.DB {
+	return dB
 }
