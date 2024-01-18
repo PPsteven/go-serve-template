d83b090070d8745b699688089c29b09c3ea84fe4
jhao <jiahao.wang@momenta.ai>
2024-01-18 16:36:53 +0800

    feat: fix

--- server/handlers/user.go
+++ server/handlers/user.go
@@ -2,14 +2,22 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-server-template/internal/db"
 	"go-server-template/server/common"
 	"strconv"
 )
 
 func GetUserByID(c *gin.Context) {
-	_, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	userID, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
+		common.ErrorResp(c, err, 400)
+		return
+	}
+	user, err := db.GetUserByID(uint(userID))
+	if err != nil {
+		common.ErrorResp(c, err, 400)
+		return
 	}
 
-	common.SuccessResp(c, nil)
+	common.SuccessResp(c, user)
 }
